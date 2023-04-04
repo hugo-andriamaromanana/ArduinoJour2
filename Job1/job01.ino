ecd0a1c1f967d949424b676fb02d142fa4eb28d2
hugo.andriamaromanana@laplateforme.io <hugo.andriamaromanana@laplateforme.io>
2023-04-04 12:19:04 +0200

    [ADD] job02

--- Job1/job01.ino
+++ Job1/job01.ino
@@ -1,9 +1,9 @@
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(2,3,4,5,6,7);
 
 void setup() {
-  lcd.begin(16, 2);
+  lcd.begin(7, 2);
   lcd.print("Hello, world!");
 }
 
--- Job2/job02.ino
+++ Job2/job02.ino
@@ -1,6 +1,15 @@
-// À l’aide de l’afficheur LCD, afficher la valeur de la tension obtenue sur des entrées.
-
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(2,3,4,5,6,7);
 
+/* display current Voltage with poe*/
+
+void setup() {
+  lcd.begin(7, 2);
+  lcd.print("Voltage:");
+}
+
+void loop() {
+  lcd.setCursor(0, 1);
+  lcd.print(analogRead(0));
+}
